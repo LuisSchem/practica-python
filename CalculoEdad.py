6806e70e53e37659c99477737c2d5ac6193620a2
LuisSchem <lschemberg@gmail.com>
2025-10-01 17:51:41 -0300

    Ahora agrego cálculo de edad básico

---
+++ CalculoEdad.py
@@ -0,0 +1,5 @@
+anioActual = 2025
+nombre = input("Hola... ¿Cuál es tu nombre? ")
+anioNacimiento = int(input(f"gracias {nombre}... ¿y tu año de nacimiento? "))
+edad = anioActual - anioNacimiento
+print(f"Bien {nombre}, entonces tu edad es de {edad} años... ")
