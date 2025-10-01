9fdbcccf027d7fe9aa0b0f5c2a0df55d7d7b5219
LuisSchem <lschemberg@gmail.com>
2025-10-01 17:11:34 -0300

    Agrego otro ejercicio

---
+++ Multiplicar_2_Numeros.py
@@ -0,0 +1,4 @@
+numero1 = int(input("Ingrese primer operando: "))
+numero2 = int(input("Ingrese segundo operando: "))
+producto = numero1 * numero2
+print(f"El producto de ambos operandos es {producto}")
