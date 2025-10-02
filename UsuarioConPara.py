2c77557c5b9c070eb37504dc235b00350563d879
LuisSchem <lschemberg@gmail.com>
2025-10-02 18:43:28 -0300

    Agrego usuario con para y testigo

---
+++ UsuarioConPara.py
@@ -0,0 +1,17 @@
+testigo=True
+
+for Intentos in range (1,4):
+    Usuario= input ("Ingrese usuario: ")
+    Contrasenia = input ("Ingrese contraseña: ")
+    if Usuario == 'admin' and Contrasenia == 'dificil123':
+        print("Login exitoso... ¡¡Bienvenido!!")
+        testigo=False
+        break        
+    else:
+        print("Credenciales incorrectas!")
+        print(f"Le quedan {3-Intentos} intentos")
+
+if testigo:
+    print("Lo siento... agotó sus intentos, vuelva a repetir el ingreso")
+
+     
